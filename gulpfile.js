4185d6b8f16d78014aa9f4a9e0536378a8ff26f0
Josh Birdwell <jbirdwell34@gmail.com>
2015-10-19 18:33:59 -0500

    update changes to gulp and data

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'),
     jade = require('gulp-jade');
 
 gulp.task('templates', function() {
-    return gulp.src('src/*.jade')
+    return gulp.src('src/index.jade')
         .pipe(data(function(file) {
             return require('./src/data.json');
         }))
